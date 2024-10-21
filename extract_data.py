7fa02420eba8f032628f900624f5cd44594039e6
Leonid <ulyanov.leo.v@gmail.com>
2024-10-22 00:49:27 +0300

    added example

--- extract_data.py
+++ extract_data.py
@@ -1,13 +1,21 @@
 import geopandas as gpd
 import osmnx as ox
 
-# Определение координаты интересующей области
-north, south, east, west = 51.64, 51.63, 39.11, 39.08
+# Разные примеры местностей
 
-bbox = [north, south, east, west]
-# Загрузка карты в виде графа
-graph = ox.graph_from_bbox(*bbox, network_type='drive')
+# 1. Дачный посёлок Анжелики:
+# north, south, east, west = 51.64, 51.63, 39.11, 39.08
+# bbox = [north, south, east, west]
+# graph = ox.graph_from_bbox(*bbox, network_type='drive')
 
-ox.plot_graph(graph)
+# 2. Тепличный, Советский район:
+graph = ox.graph_from_place("Тепличный, Voronezh, Russia")
+
+# ox.plot_graph(graph)
 nodes, edges = ox.graph_to_gdfs(graph)
 
+extracted = nodes.loc[[1029821343]]
+extracted["y"] = 60
+nodes.loc[[1029821343]] = extracted
+graph = ox.graph_from_gdfs(nodes, edges)
+ox.plot_graph(graph)
