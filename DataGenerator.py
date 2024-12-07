8a87279650137f959d6a6e1e82d9de954d146b2b
leonid <ulyanov.leo.v@gmail.com>
2024-12-07 16:32:49 +0300

    added comments + formatting

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,25 +1,36 @@
 import os
 import random
+from typing import Tuple
 import osmnx as ox
 import networkx as nx
-from geopy.distance import geodesic as gd
+from geopy.distance import geodesic as gd  # type: ignore
 import json
-from TMS_request import TMSRequest
+from TMSRequest import TMSRequest
 
 
 class DataGenerator:
     def __init__(self, config_path: str = "config.json", **kwargs):
-        self.__load_config(config_path)
-        self.main_routes = []
-        self.false_routes = []
+        """Конструктор класса, которому в именованных аргументах передаётся
+        либо название местности, либо точные координаты местности.
+        Если было передано название, то происходит обращение к базе данных OSM,
+        где потом извлекаются точные координаты. На основе координат строится граф дорог.
 
-        # border box, задаёт границы для исследуемой местности
-        # через координаты противоположных углов
+
+        Args:
+            config_path (str, optional): Путь к файлу конфигурации. По умолчанию стоит "config.json".
+
+        Raises:
+            Exception: Не были переданы ни название местности, ни его координаты.
+        """
+
+        self.__load_config(config_path)
+        self.main_routes = []  # Исходные маршруты
+        self.false_routes = []  # Искажённые маршруты
 
         if "place_name" in kwargs.keys():
-            self.__place_bbox = list(ox.geocode_to_gdf(kwargs["place_name"])
-                                     .geometry
-                                     .total_bounds)
+            self.__place_bbox = list(
+                ox.geocode_to_gdf(kwargs["place_name"]).geometry.total_bounds
+            )
 
         elif "place_bbox" in kwargs.keys():
             self.__place_bbox = kwargs["place_bbox"]
@@ -28,29 +39,55 @@ class DataGenerator:
                 "Укажите название места согласно базе данных OSM либо координаты местности."
             )
 
-        self.__graph = ox.graph_from_bbox(self.__place_bbox)  # граф местности
+        self.__graph = ox.graph_from_bbox(self.__place_bbox)  # Граф дорог местности
 
     def __load_config(self, file_path: str) -> None:
+        """Загрузка данных о константах через файл конфигурации.
+
+        Args:
+            file_path (str): Путь к файлу конфигурации.
+        """
+
         with open(file_path, "r") as file:
             config = json.load(file)
-            self.__data_amount = config["data_amount"]
-            self.__min_segment = config["min_segment"]
-            self.__max_segment = config["max_segment"]
-            self.__min_offset = config["min_offset"]
-            self.__max_offset = config["max_offset"]
+            self.__data_amount = config["data_amount"]  # Размер генерируемой выборки
+            self.__min_segment = config[
+                "min_segment"
+            ]  # Минимальное значение отрезка для создания отклонения
+            self.__max_segment = config[
+                "max_segment"
+            ]  # Максимальное значение отрезка для создания отклонения
+            self.__min_offset = config["min_offset"]  # Минимальное отклонение
+            self.__max_offset = config["max_offset"]  # Максимальное отклонение
 
     def __generate_main_route(self) -> list:
+        """Генерация одного исходного маршрута на основе алгоритма А*.
+
+        Returns:
+            list: Список идентификаторов узлов графа.
+            Сами идентификаторы заданы заранее на основе базы OSM.
+        """
+
         # Выбор случайных точек в графе
         keys = list(self.__graph.nodes.keys()).copy()
         start = random.choice(keys)
         keys.remove(start)
         end = random.choice(keys)
 
-        # Поиск кратчайшего пути с помощью алгоритма А*
+        # Поиск кратчайшего пути
         main_route = nx.astar_path(self.__graph, start, end, weight="length")
         return main_route
 
-    def __get_one_false_route(self, main_route: list) -> (nx.Graph, list):
+    def __get_one_false_route(self, main_route: list) -> Tuple[nx.Graph, list]:
+        """Генерация одного искажённого маршрута на основе исходного.
+
+        Args:
+            list: Исходный маршрут.
+
+        Returns:
+            Tuple[nx.Graph, list]: Кортеж, внутри которого помещён изменённый граф и полученный маршрут.
+        """
+
         path = main_route
         G = self.__graph
         new_nodes = [path[0]]
@@ -109,23 +146,67 @@ class DataGenerator:
             new_nodes.append(path[i + 1])
         return G, new_nodes
 
-    def save_main_route(self, save_folder: str) -> None:
+    def __transform_bbox(self, bbox: list) -> dict:
+        """
+        Преобразование списка координат в словарь.
+        Args:
+            bbox (list): Исходный список.
+
+        Returns:
+            dict: Преобразованный словарь.
+        """
+        copy = bbox.copy()
+        copy.sort()
+
+        return {
+            "lat1": min(copy[2], copy[3]),
+            "lon1": min(copy[0], copy[1]),
+            "lat2": max(copy[2], copy[3]),
+            "lon2": max(copy[0], copy[1]),
+        }
+
+    def __save_main_route(self, save_folder: str) -> None:
+        """Генерация и сохранение исходного маршрута в указанную папку в виде единого изображения,
+        а также в виде отдельных частей размером 256х256.
+        Получение изображения происходит с помощью запроса по протоколу TMS.
+
+        Args:
+            save_folder (str): Путь к папке для сохранения.
+        """
+        request = TMSRequest()
         main_route = self.__generate_main_route()
         self.main_routes.append(main_route)
         index = len(self.main_routes)
-        request = TMSRequest()
+        bbox = self.__transform_bbox(self.__place_bbox)
+
         request.get(
-            self.__place_bbox, self.__graph, main_route,
-            save_folder, f"main{index}", "png"
+            bbox,
+            self.__graph,
+            main_route,
+            save_folder,
+            f"main{index}",
+            "png",
         )
 
-    def save_false_route(self, save_folder: str) -> None:
+    def __save_false_route(self, save_folder: str) -> None:
+        """Генерация и сохранение искажённого маршрута в указанную папку в виде единого изображения,
+        а также в виде отдельных частей размером 256х256.
+        Получение изображения происходит с помощью запроса по протоколу TMS.
+
+        Args:
+            save_folder (str): Путь к папке для сохранения.
+        """
+
         request = TMSRequest()
         index = len(self.main_routes)
-        false_graph, false_route = self.__get_one_false_route(self.main_routes[index - 1])
+        false_graph, false_route = self.__get_one_false_route(
+            self.main_routes[index - 1]
+        )
         self.false_routes.append((false_graph, false_route))
+        bbox = self.__transform_bbox(self.__place_bbox)
+
         request.get(
-            self.__place_bbox,
+            bbox,
             false_graph,
             false_route,
             save_folder,
@@ -134,14 +215,23 @@ class DataGenerator:
         )
 
     def save_data(self, save_folder: str) -> None:
+        """Генерация выборки с заранее указанным размером
+        и сохранение изображений маршрутов в указанную папку.
+
+        Args:
+            save_folder (str): Путь к папке для сохранения.
+        """
+
         for i in range(1, self.__data_amount + 1):
             route_folder = os.path.join(save_folder, f"route{i}")
             os.makedirs(route_folder, exist_ok=True)
-            self.save_main_route(route_folder)
-            self.save_false_route(route_folder)
+            self.__save_main_route(route_folder)
+            self.__save_false_route(route_folder)
 
 
 if __name__ == "__main__":
     os.makedirs("images", exist_ok=True)
-    data_generator = DataGenerator(place_bbox=[39.121447, 51.646002, 39.135578, 51.653782])
+    data_generator = DataGenerator(
+        place_bbox=[39.121447, 51.646002, 39.135578, 51.653782]
+    )
     data_generator.save_data("images")
---
+++ TMSRequest.py
@@ -0,0 +1,257 @@
+import math
+from time import sleep
+from typing import Tuple
+import requests
+from PIL import Image, ImageDraw
+from io import BytesIO
+import networkx as nx
+import os
+
+
+class TMSRequest:
+    def __init__(self):
+        self.__tile_size = 256  # Размер одной "плитки" - 256 пикселей
+        self.__zoom_size = 15  # Параметр масштабирования карты - 15
+
+        # Координаты верхней правой плитки
+        self.__x1_tile = 0
+        self.__y1_tile = 0
+
+        # Координаты нижней левой плитки
+        self.__x2_tile = 0
+        self.__y2_tile = 0
+
+        self.__map_img = None  # Полное изображение карты
+
+    @staticmethod
+    def __lat_lon_to_tile(lat: float, lon: float, zoom: int) -> Tuple[int, int]:
+        """Конвертация из географических координат в координаты согласно протоколу TMS.
+
+        Args:
+            lat (float): Ширина.
+            lon (float): Долгота.
+            zoom (int): Параметр масштабирования.
+
+        Returns:
+            Tuple[int, int]: Координаты согласно TMS.
+        """
+
+        n = 2**zoom
+        x_tile = int((lon + 180) / 360 * n)
+        y_tile = int(
+            (
+                1
+                - (
+                    math.log(
+                        math.tan(math.radians(lat)) + 1 / math.cos(math.radians(lat))
+                    )
+                    / math.pi
+                )
+            )
+            / 2
+            * n
+        )
+        return x_tile, y_tile
+
+    @staticmethod
+    def __download_tile(x: int, y: int, zoom: int) -> Image.Image:
+        """Загрузка одной плитки из сервера OSM.
+
+        Args:
+            x (int): Координата плитки по Х.
+            y (int): Координата плитки по У.
+            zoom (int): Параметр масштабирования.
+
+        Raises:
+            Exception: Ответ от сервера не был получен либо был получен некорректно.
+
+        Returns:
+            Image.Image: Изображение в виде объекта библиотеки Pillow.
+        """
+
+        url = f"https://tile.openstreetmap.org/{zoom}/{x}/{y}.png"
+        headers = {"User-Agent": "Chrome/58.0.3029.110"}
+        sleep(1)
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return Image.open(BytesIO(response.content))
+        else:
+            raise Exception(
+                f"Error downloading tile {x}/{y} at zoom {zoom}: {response.status_code}"
+            )
+
+    @staticmethod
+    def __geo_to_pixel(
+        lat: float, lon: float, x_tile: int, y_tile: int, zoom: int
+    ) -> Tuple[int, int]:
+        """Преобразование географических координат в пиксели на экране согласно размерам изображения.
+
+        Args:
+            lat (float): Ширина точки.
+            lon (float): Долгота точки.
+            x_tile (int): Координата плитки Х.
+            y_tile (int): Координата плитки У.
+            zoom (int): Параметр машстабирования.
+
+        Returns:
+            Tuple[int, int]: Координаты точки на экране.
+        """
+
+        n = 2.0**zoom
+        x = (lon + 180.0) / 360.0 * n
+        y = (
+            (
+                1.0
+                - math.log(
+                    math.tan(math.radians(lat)) + 1 / math.cos(math.radians(lat))
+                )
+                / math.pi
+            )
+            / 2.0
+            * n
+        )
+        pixel_x = (x - x_tile) * 256
+        pixel_y = (y - y_tile) * 256
+        return pixel_x, pixel_y
+
+    def __set_tiles_coords(
+        self, lat1: float, lon1: float, lat2: float, lon2: float, zoom: int
+    ):
+        """
+        Получение координат для двух плиток, находящихся в противоположных углах карты,
+        на основе координат противоположных точек, составляющих местность.
+
+        Args:
+            lat1 (float): Ширина 1-й точки.
+            lon1 (float): Долгота 1-й точки.
+            lat2 (float): Ширина 2-й точки.
+            lon2 (float): Долгота 2-й точки.
+            zoom (int): Параметр масштабирования.
+        """
+
+        self.__x1_tile, self.__y1_tile = self.__lat_lon_to_tile(lat1, lon1, zoom)
+        self.__x2_tile, self.__y2_tile = self.__lat_lon_to_tile(lat2, lon2, zoom)
+
+        # Создание пустого изображения для вставки более мелких изображений
+
+        width = int(math.fabs(self.__x2_tile - self.__x1_tile) + 1) * self.__tile_size
+        height = int(math.fabs(self.__y2_tile - self.__y1_tile) + 1) * self.__tile_size
+        self.__map_img = Image.new("RGB", (width, height))
+
+        self.__x1_tile, self.__x2_tile = min(self.__x1_tile, self.__x2_tile), max(
+            self.__x1_tile, self.__x2_tile
+        )
+        self.__y1_tile, self.__y2_tile = min(self.__y1_tile, self.__y2_tile), max(
+            self.__y1_tile, self.__y2_tile
+        )
+
+    def __fill_img_with_tiles(self, zoom: int):
+        """Заполнение полного изображения плитками, полученными с помощью запроса TMS.
+
+        Args:
+            zoom (int): Параметр масштабирования.
+        """
+
+        for x in range(self.__x1_tile, self.__x2_tile + 1):
+            for y in range(self.__y1_tile, self.__y2_tile + 1):
+                tile_image = self.__download_tile(x, y, zoom)
+                if tile_image:
+                    self.__map_img.paste(
+                        tile_image,
+                        (
+                            (x - self.__x1_tile) * self.__tile_size,
+                            (y - self.__y1_tile) * self.__tile_size,
+                        ),
+                    )
+
+    def __draw_route_on_map(
+        self, route_coords: list, zoom: int, save_folder: str, name: str, extension: str
+    ):
+        """Отрисовка указанного маршрута на изображении карты
+        и сохранение полученного изображения.
+
+        Args:
+            route_coords (list): Маршрут, представленный списком координат.
+            zoom (int): Параметр масштабирования.
+            save_folder (str): Путь к папке для сохранения.
+            name (str): Название файла для сохранения.
+            extension (str): Расширение файла.
+        """
+
+        draw = ImageDraw.Draw(self.__map_img)
+        for i in range(len(route_coords) - 1):
+            x1, y1 = route_coords[i]
+            x2, y2 = route_coords[i + 1]
+
+            x1_px, y1_px = self.__geo_to_pixel(
+                x1, y1, self.__x1_tile, self.__y1_tile, zoom
+            )
+            x2_px, y2_px = self.__geo_to_pixel(
+                x2, y2, self.__x1_tile, self.__y1_tile, zoom
+            )
+            draw.line([(x1_px, y1_px), (x2_px, y2_px)], fill="blue", width=3)
+
+        os.makedirs(os.path.join(save_folder, name), exist_ok=True)
+        self.__map_img.save(f"{save_folder}/{name}/{name}.{extension}")
+
+    def __split_img_into_tiles(
+        self, save_folder: str, name: str, extension: str
+    ) -> None:
+        """Разделение изображения карты на плитки и сохранение в указанную папку.
+
+        Args:
+            save_folder (str): Путь к папке для сохранения.
+            name (str): Название файла для сохранения.
+            extension (str): Расширение файла.
+        """
+
+        os.makedirs(save_folder, exist_ok=True)
+        img = Image.open(os.path.join(save_folder, name, f"{name}.{extension}"))
+        img_width, img_height = img.size
+
+        for i in range(0, img_width, self.__tile_size):
+            for j in range(0, img_height, self.__tile_size):
+                box = (i, j, i + self.__tile_size, j + self.__tile_size)
+                cropped = img.crop(box)
+                cropped.save(
+                    os.path.join(save_folder, name, f"{name}_{i}_{j}.{extension}")
+                )
+
+    def get(
+        self,
+        geo_coords: dict,
+        graph: nx.Graph,
+        node_ids: list,
+        save_folder: str,
+        route_name: str,
+        extension: str,
+    ):
+        """Объединение предыдущих методов в единый запрос.
+
+        Args:
+            geo_coords (dict): Координаты местности.
+            graph (nx.Graph): Граф дорог местности.
+            node_ids (list): Маршрут для отрисовки в виде списка идентификаторов вершин.
+            save_folder (str): Путь к папке для сохранения.
+            route_name (str): Название файла для сохранения.
+            extension (str): Расширение файла.
+        """
+
+        lat1, lon1, lat2, lon2 = (
+            geo_coords["lat1"],
+            geo_coords["lon1"],
+            geo_coords["lat2"],
+            geo_coords["lon2"],
+        )
+
+        zoom = self.__zoom_size
+        self.__set_tiles_coords(lat1, lon1, lat2, lon2, zoom)
+        self.__fill_img_with_tiles(zoom)
+
+        route_coords = [(graph.nodes[n]["y"], graph.nodes[n]["x"]) for n in node_ids]
+        self.__draw_route_on_map(route_coords, zoom, save_folder, route_name, extension)
+        self.__split_img_into_tiles(save_folder, route_name, extension)
+
+
+if __name__ == "__main__":
+    pass
